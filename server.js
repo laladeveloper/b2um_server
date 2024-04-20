43d030f95fe5e4d2b32a9062b4dbb063c205d854
laladeveloper <hafizsaqlainmujtaba@gmail.com>
2024-04-21 00:11:52 +0500

    cors added

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ const corsOptions = {
 };
 
 // Apply CORS middleware with options
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(express.json());
 // app.use(bodyParser.urlencoded({ extended: true,}));
