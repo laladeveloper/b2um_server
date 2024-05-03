74dca9a6a65c36cd360d33391ebe0d8d36c3621a
laladeveloper <hafizsaqlainmujtaba@gmail.com>
2024-05-03 16:22:36 +0500

    vercel origin added

--- app.js
+++ app.js
@@ -13,43 +13,40 @@ const app = express();
 connectDB();
 app.use(cookieParser());
 app.use(express.json());
-const allowedOrigins = [
-  "http://localhost:5173",
-  "http://localhost:4000",
-  "https://b2um-rl1tv8zcd-laladevelopers-projects.vercel.app",
-  "https://b2um.vercel.app",
-  "https://b2um-server.vercel.app",
-];
+// const allowedOrigins = [
+//   "http://localhost:5173",
+//   "http://localhost:4000",
+//   "https://b2um-rl1tv8zcd-laladevelopers-projects.vercel.app",
+//   "https://b2um.vercel.app",
+//   "https://b2um-server.vercel.app",
+// ];
 
-const corsOptions = {
-  origin: function (origin, callback) {
-    // Check if the origin is in the list of allowed origins or if it's undefined (i.e., not set by the browser)
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS understand saqlain"));
-    }
-  },
-  credentials: true,
-};
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     // Check if the origin is in the list of allowed origins or if it's undefined (i.e., not set by the browser)
+//     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error("Not allowed by CORS understand saqlain"));
+//     }
+//   },
+//   credentials: true,
+// };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 
-// app.use(cors({credentials:true})); // Enable CORS for all origins
-
+app.use(
+  cors({
+    origin: "https://b2um.vercel.app", // Replace with your frontend origin
+    credentials: true, // Allow cookies to be sent with the request
+  })
+);
 app.use("/api/user", userRoute);
 app.use("/api/admin", adminRoute);
 
 app.get("/", (req, res) => {
   res.send("Saqlain your code is working");
 });
-// for production mode from the server side 
-// app.use(express.static(path.join(__dirname, "dist")));
-
-// app.get("*", (req, res) => {
-//   res.sendFile(path.resolve(__dirname, "dist/index.html"));
-// });
-// D:\Web Dev\clients\Bunty Bhai\final website\backendServer\dist\index.html
 
 const port = process.env.PORT;
 app.listen(port, function listening(req, res) {
